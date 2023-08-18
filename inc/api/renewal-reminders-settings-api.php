4aebda30bb007f14c8ef4950183be9c3190016a9
dlnieves <dlnieves@yahoo.es>
2023-08-19 01:29:52 +0200

    domain init

--- inc/api/renewal-reminders-settings-api.php
+++ inc/api/renewal-reminders-settings-api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package  RenewalReminders
  */
@@ -6,7 +7,7 @@
 
 class SPRRSettingsApi
 {
-    public $admin_pages = array();
+	public $admin_pages = array();
 
 	public $settings = array();
 
@@ -16,16 +17,21 @@ class SPRRSettingsApi
 
 	public function sprr_register()
 	{
-		if ( ! empty($this->admin_pages) ) {
-			add_action( 'admin_menu', array( $this, 'sprr_addAdminMenu' ) );
+		//init i18n system as early as possible 
+		add_action('plugins_loaded', array($this, 'load_plugin_textdomain'));
+		
+		if (!empty($this->admin_pages)) {
+			add_action('admin_menu', array($this, 'sprr_addAdminMenu'));
 		}
 
-		if ( !empty($this->settings) ) {
-			add_action( 'admin_init', array( $this, 'sprr_registerCustomFields' ) );
+		if (!empty($this->settings)) {
+			add_action('admin_init', array($this, 'sprr_registerCustomFields'));
 		}
+
+		
 	}
 
-	public function sprr_addPages( array $pages )
+	public function sprr_addPages(array $pages)
 	{
 		$this->admin_pages = $pages;
 
@@ -34,26 +40,26 @@ class SPRRSettingsApi
 
 	public function sprr_addAdminMenu()
 	{
-		foreach ( $this->admin_pages as $page ) {
-			add_menu_page( $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position'] );
+		foreach ($this->admin_pages as $page) {
+			add_menu_page($page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position']);
 		}
-    }
-    
-    public function sprr_setSettings( array $settings )
+	}
+
+	public function sprr_setSettings(array $settings)
 	{
 		$this->settings = $settings;
 
 		return $this;
 	}
 
-	public function sprr_setSections( array $sections )
+	public function sprr_setSections(array $sections)
 	{
 		$this->sections = $sections;
 
 		return $this;
 	}
 
-	public function sprr_setFields( array $fields )
+	public function sprr_setFields(array $fields)
 	{
 		$this->fields = $fields;
 
@@ -63,19 +69,30 @@ class SPRRSettingsApi
 	public function sprr_registerCustomFields()
 	{
 		// register setting
-		foreach ( $this->settings as $setting ) {
-			register_setting( $setting["option_group"], $setting["option_name"], ( isset( $setting["callback"] ) ? $setting["callback"] : '' ) );
+		foreach ($this->settings as $setting) {
+			register_setting($setting["option_group"], $setting["option_name"], (isset($setting["callback"]) ? $setting["callback"] : ''));
 		}
 
 		// add settings section
-		foreach ( $this->sections as $section ) {
-			add_settings_section( $section["id"], $section["title"], ( isset( $section["callback"] ) ? $section["callback"] : '' ), $section["page"] );
+		foreach ($this->sections as $section) {
+			add_settings_section($section["id"], $section["title"], (isset($section["callback"]) ? $section["callback"] : ''), $section["page"]);
 		}
 
 		// add settings field
-		foreach ( $this->fields as $field ) {
-			add_settings_field( $field["id"], $field["title"], ( isset( $field["callback"] ) ? $field["callback"] : '' ), $field["page"], $field["section"], ( isset( $field["args"] ) ? $field["args"] : '' ) );
+		foreach ($this->fields as $field) {
+			add_settings_field($field["id"], $field["title"], (isset($field["callback"]) ? $field["callback"] : ''), $field["page"], $field["section"], (isset($field["args"]) ? $field["args"] : ''));
 		}
-    }
-    
+	}
+
+	/**
+	 * Load domain
+	 */
+	public function load_plugin_textdomain()
+	{
+		load_plugin_textdomain(
+			'subscriptions-renewal-reminders',
+			false,
+			dirname(dirname(plugin_basename(__FILE__))) . '/languages/'
+		);
+	}
 }
